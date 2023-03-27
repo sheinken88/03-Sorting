b2e08eaac3b87cabde15c1451e6b3cfcf483cb55
PC <heinken.sebastian@gmail.com>
2023-03-27 19:49:44 -0300

    first commit 03-Sorting

---
+++ bubblesort.js
@@ -0,0 +1,14 @@
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(bubbleSort([5, 3, 4, 1, 2]));
